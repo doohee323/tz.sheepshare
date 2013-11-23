e0a0c3f58ddfe6511deb5c89e957819a2d682b89
doohee <doohee323@gmail.com>
2013-11-23 22:43:35 +0900

    .

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -3,7 +3,7 @@
 var config = {
 //	url : 'http://localhost\\:3000',
 //	url: 'http://sheeprails.herokuapp.com',
-	url : 'http://localhost\\:7001/rest',
+	url : 'http://localhost\\:7001/tz.sheepshare/rest',
 	// url : '/pattern/pt42/masterdetail',
 	server: 'spring' // spring, rails,
 };
@@ -13,22 +13,22 @@ angular.module('sheepwebApp', ['ngResource'])
 	.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when('/', {
-		redirectTo : '/centers'
+		redirectTo : '/tz.sheepshare/centers'
 	})
-	.when('/centers', {
+	.when('/tz.sheepshare/centers', {
 		controller : 'CentersCtrl',
 		templateUrl : './views/centers.html'
 	})
-	.when('/center/:id', {
+	.when('/tz.sheepshare/center/:id', {
 		controller : 'CentersCtrl',
 		templateUrl : './views/centers.html'
 	})	
-	.when('/regions/:id', {
+	.when('/tz.sheepshare/regions/:id', {
 		controller : 'RegionsCtrl',
 		templateUrl : '/views/regions.html'
 	})
 	.otherwise({
-		redirectTo : '/centers'
+		redirectTo : '/tz.sheepshare/centers'
 	});
 	
 	$locationProvider.html5Mode(true).hashPrefix('!');
